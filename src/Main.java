1686949bcd092ab7587fc019d560d4aa21fb37ba
Evgeniy Malygin <black2001@inbox.ru>
2022-08-19 14:29:14 +0300

    Решение первой задачи

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        // целочисленный тип переменной
+        byte varByte = 10;
+        short varShort = 11;
+        int varInt = 12;
+        long varLong = 100_000_000;
 
-        System.out.println("Hello world!");
+        // тип переменной с плавающей точкой
+        float varFloat = 1.56f;
+        double varDouble = -123.55;
+
+        //символьная переменная
+        char varChar = 'F';
+
+        //Логическая переменная
+
+        boolean varBoolean = true;
+
+
+
     }
 }
